813a908cc71f3b6d12c8480a577f52119d8df494
Keshav Moraniya <101967814+Keshav0517@users.noreply.github.com>
2024-06-18 16:44:43 +0530

    Add files via upload

---
+++ Arrays/Array/Remove_Duplicates_II.java
@@ -0,0 +1,16 @@
+package Array.Array2D;
+
+//https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/description/
+public class Remove_Duplicates_II {
+    public static void main(String[] args) {
+        int[] arr={1,1,1,2,2,3,3,3};
+        int j=2;
+        for (int i=2;i<arr.length;i++){
+            if (arr[i] != arr[j-2]){
+                arr[j]=arr[i];
+                j++;
+            }
+        }
+        System.out.println("Unique elements:"+j);
+    }
+}
